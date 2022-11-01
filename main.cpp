8aa90399edf890800abec877ed9b79be03b2f071
lcy <291462871@qq.com>
2022-11-01 17:31:14 +0800

    add thread_pool

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include "source/mysql_connection.h"
 #include <iostream>
 
+
+
 int main() {
-    // MysqlConnection conn("127.0.0.1", "root", "123456", "lcy");
     auto& con = MysqlConnection::GetInstance();
     if (con.Init("127.0.0.1", 3306, "root", "123456", "lcy")) {
         printf("mysql connect success!\n");    
     }
-    
 
     return 0;
     
