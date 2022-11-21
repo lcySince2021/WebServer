4613baf8d8ae457ea0057f6c0b7d6cdf2b827200
lcy <291462871@qq.com>
2022-11-21 15:51:42 +0800

    .

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "source/mysql_connection.h"
 //#include "source/http.h"
 #include "source/thread_pool.h"
+#include "source/db_connection_pool.h"
 #include "pthread.h"
 #include <iostream>
 #include <unistd.h>
@@ -11,20 +12,23 @@ using namespace std;
 
 
 int main() {
-    ThreadPool pool(4);
-    std::vector<std::future<int>> results;
-    int num = 15;
-    while (num--) {
-        results.emplace_back(pool.enqueue([](int num) { 
-                printf("thread id: %llu\n", std::this_thread::get_id());
-                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-                return num; 
-                }, 4));
-    }
-    for (auto& result : results) {
-        // std::cout << result.get() << std::endl;
-        result.get();
-    }
+    DbConnectionPool::GetInstance().SetPararmeter(8, "127.0.0.1", 3306, "root", "123456", "lcy");
+    auto db_pool = DbConnectionPool::GetInstance();
+    printf("max connect count: %d\n", db_pool.GetConnectCount());
+    // ThreadPool pool(4);
+    // std::vector<std::future<int>> results;
+    // int num = 15;
+    // while (num--) {
+    //     results.emplace_back(pool.enqueue([](int num) { 
+    //             printf("thread id: %llu\n", std::this_thread::get_id());
+    //             std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    //             return num; 
+    //             }, 4));
+    // }
+    // for (auto& result : results) {
+    //     // std::cout << result.get() << std::endl;
+    //     result.get();
+    // }
     return 0;
     
 }
--- source/db_connection_pool.h
+++ source/db_connection_pool.h
@@ -6,12 +6,36 @@
 
 class DbConnectionPool {
 public:
-    DbConnectionPool(int count);
-    ~DbConnectionPool();
-    void SetPararmeter(int max_connect_num, const std::string& host, int port,
-                        const std::string& username, const std::string& password,
-                        const std::string database);
+    static DbConnectionPool& GetInstance() {
+        static DbConnectionPool instance;
+        return instance;
+    }
+    ~DbConnectionPool() {}
+    void SetPararmeter(int max_connect_num, const std::string& host, int port, const std::string& username, const std::string& password, const std::string database) {
+        max_connect_num_ = max_connect_num_;
+        host_ = host;
+        port_ = port;
+        username_ = username;
+        password_ = password;
+    }
+    int GetMaxConnectCount() { return max_connect_num_; }
+    int GetConnectCount() { return connect_num_; }
 
+
+private:
+    DbConnectionPool() {
+        for (int i = 0; i < max_connect_num_; ++i) {
+            MYSQL* mysql;
+            if (!mysql_init(mysql)) {
+                return;
+            }
+            if (!mysql_real_connect(mysql, host_.c_str(), username_.c_str(), password_.c_str(), database_.c_str(), port_, NULL, 0)) {
+                return;
+            }
+            connection_lists_.push_back(mysql);
+        }
+    }
+    
 
 private:
     int max_connect_num_;
@@ -21,7 +45,7 @@ private:
     std::string username_;
     std::string password_;
     std::string database_;
-    MysqlConnection mysql_connection_;
+    // MysqlConnection mysql_connection_;
     std::list<MYSQL*> connection_lists_;
 };
 
