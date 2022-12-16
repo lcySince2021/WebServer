a13ffe9604bbb556fd8501bc8ce4dad5fde66401
lcy <291462871@qq.com>
2022-12-16 14:04:58 +0800

    .

--- main.cpp
+++ main.cpp
@@ -15,20 +15,19 @@ int main() {
     int count = db_pool->GetFree();
     cout << count << endl;
     auto conn = db_pool->GetFree();
-    // ThreadPool pool(4);
-    // std::vector<std::future<int>> results;
-    // int num = 15;
-    // while (num--) {
-    //     results.emplace_back(pool.enqueue([](int num) { 
-    //             printf("thread id: %llu\n", std::this_thread::get_id());
-    //             std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    //             return num; 
-    //             }, 4));
-    // }
-    // for (auto& result : results) {
-    //     // std::cout << result.get() << std::endl;
-    //     result.get();
-    // }
+    ThreadPool pool(4);
+    std::vector<std::future<int>> results;
+    int num = 15;
+    while (num--) {
+        results.emplace_back(pool.enqueue([](int num) { 
+                printf("thread id: %llu\n", std::this_thread::get_id());
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+                return num; 
+                }, 4));
+    }
+    for (auto& result : results) {
+        result.get();
+    }
     return 0;
     
 }
